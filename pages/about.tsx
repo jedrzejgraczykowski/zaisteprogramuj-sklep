59c9cbc4fe5b76bb2e427a1b2a7a1ce180ff58c0
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2022-02-21 23:47:53 +0100

    Clear the project

--- pages/about.tsx
+++
@@ -1,7 +0,0 @@
-const AboutPage = () => {
-  return (
-    <div>Hello World!</div>
-  );
-}
-
-export default AboutPage;
--- pages/users/index.tsx
+++
@@ -1,7 +0,0 @@
-const UserIndexPage = () => {
-  return (
-    <div>UserIndexPage</div>
-  );
-}
-
-export default UserIndexPage;
