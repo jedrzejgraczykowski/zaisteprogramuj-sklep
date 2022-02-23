740cece9d5883aa147b9e93a356c7764926b998f
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2022-02-23 23:01:23 +0100

    Refactor the nav in the Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,12 +6,12 @@ const Header = () => {
 
   return (
     <header className="max-w-7xl mx-auto w-full">
-      <nav className="bg-gray-700 text-white px-4 py-2">
+      <nav className="bg-gray-700 text-white font-medium px-4 py-2">
         <Link href="/">
-          <a className={`pr-4 hover:font-black ${router.pathname == '/' ? "font-black" : ""}`}>Główna</a>
+          <a className={`mr-4 hover:selected ${router.pathname == '/' ? "selected" : ""}`}>Główna</a>
         </Link>
         <Link href="/about">
-          <a className={`px-2 hover:font-black ${router.pathname == '/about' ? "font-black" : ""}`}>About</a>
+          <a className={`mr-4 hover:selected ${router.pathname == '/about' ? "selected" : ""}`}>About</a>
         </Link>
       </nav>
     </header>
