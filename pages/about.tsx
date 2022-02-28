d91dfdd454d4aa0255b0630b35b830bd6435b805
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2022-02-28 22:48:14 +0100

    Divide the project to smaller components - tutorial

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,19 +1,22 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Main } from "../components/Main";
 
 const About = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header/>
-      <main className="flex-grow max-w-2xl mx-auto p-6">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus velit quis mauris fermentum mollis.
-        Donec at justo hendrerit, pretium sapien eget, cursus tortor. Aenean consequat et lacus id tristique.
-        Ut nec ex in nulla porttitor facilisis facilisis vestibulum augue. Pellentesque odio augue, tincidunt sit amet libero a, venenatis mollis tortor.
-        Aenean ut tellus a nisl ultrices congue vitae id turpis. Nullam fringilla tellus sit amet dictum bibendum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-        Phasellus blandit eros in ex tristique ullamcorper. Nullam lectus nisl, suscipit consectetur lorem non, tempus auctor lorem.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </main>
+      <Main>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus velit quis mauris fermentum mollis.
+          Donec at justo hendrerit, pretium sapien eget, cursus tortor. Aenean consequat et lacus id tristique.
+          Ut nec ex in nulla porttitor facilisis facilisis vestibulum augue. Pellentesque odio augue, tincidunt sit amet libero a, venenatis mollis tortor.
+          Aenean ut tellus a nisl ultrices congue vitae id turpis. Nullam fringilla tellus sit amet dictum bibendum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Phasellus blandit eros in ex tristique ullamcorper. Nullam lectus nisl, suscipit consectetur lorem non, tempus auctor lorem.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </Main>
       <Footer/>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,25 @@
+import { ReactNode } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Product } from "../components/Produxt";
+import { Main } from "../components/Main";
+
+
+const DATA = {
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac ipsum ac nisl hendrerit pretium sit amet non felis. Donec condimentum eget mauris et cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas faucibus tincidunt sem eget pretium. Vivamus in nibh suscipit, volutpat dolor eget, mollis velit.Proin tristique nunc ac nisi gravida, id vestibulum odio dignissim. Mauris id lacus sodales, pretium neque a, dapibus magna. Vestibulum fringilla velit ac elit vehicula euismod.',
+  imageUrl: "https://picsum.photos/id/1065/536/364",
+  imageAlt: "Słoneczna uliczka",
+  rating: 4.5,
+};
+
 
 const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header/>
-      <main className="flex-grow max-w-2xl mx-auto grid p-6 gap-6 sm:grid-cols-2">
-        <img
-          src="https://picsum.photos/id/1065/536/364"
-          alt="Słoneczna uliczka"
-        />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac ipsum ac nisl hendrerit pretium sit amet non felis.
-          Donec condimentum eget mauris et cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-          Maecenas faucibus tincidunt sem eget pretium. Vivamus in nibh suscipit, volutpat dolor eget, mollis velit.
-          Proin tristique nunc ac nisi gravida, id vestibulum odio dignissim. Mauris id lacus sodales, pretium neque a, dapibus magna.
-          Vestibulum fringilla velit ac elit vehicula euismod.
-        </p>
-      </main>
+      <Main>
+        <Product data={DATA} />
+      </Main>
       <Footer/>
     </div>
   );
