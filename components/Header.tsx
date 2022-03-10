b585473c10c1073cfe05c6e9703656dab0d88f86
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2022-03-10 21:59:12 +0100

    Add a logo to the shop

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Logo } from "./Logo";
 
 const Header = () => {
   const router = useRouter();
 
   return (
     <header className="max-w-7xl mx-auto w-full">
-      <nav className="bg-gray-700 text-white font-medium px-4 py-2">
+      <nav className="flex bg-gray-700 text-white font-medium px-4 py-4">
+        <Logo />
         <Link href="/">
-          <a className={`mr-4 hover:selected ${router.pathname == '/' ? "selected" : ""}`}>Home</a>
+          <a className={`mr-4 mt-1 hover:selected ${router.pathname == '/' ? "selected" : ""}`}>Home</a>
         </Link>
         <Link href="/products">
-          <a className={`mr-4 hover:selected ${router.pathname == '/products' ? "selected" : ""}`}>Products</a>
+          <a className={`mr-4 mt-1 hover:selected ${router.pathname == '/products' ? "selected" : ""}`}>Products</a>
         </Link>
         <Link href="/about">
-          <a className={`mr-4 hover:selected ${router.pathname == '/about' ? "selected" : ""}`}>About</a>
+          <a className={`mr-4 mt-1 hover:selected ${router.pathname == '/about' ? "selected" : ""}`}>About</a>
         </Link>
       </nav>
     </header>
---
+++ components/Logo.tsx
@@ -0,0 +1,5 @@
+export const Logo = () => {
+  return (
+    <div className="mr-7 text-2xl font-extrabold">Random Shop</div>
+  );
+}
