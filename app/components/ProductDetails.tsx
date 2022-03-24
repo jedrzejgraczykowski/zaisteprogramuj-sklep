ec706efb930ffa68478e3dd9c5aa8f7f7b86e7ac
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2022-03-24 23:42:09 +0100

    05 add next-seo and use it in the ProductDetails

--- app/components/ProductDetails.tsx
+++ app/components/ProductDetails.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Rating } from "./Rating";
 import ReactMarkdown from "react-markdown";
 import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 export interface ProductDetails {
   id: number,
@@ -20,9 +21,24 @@ interface ProductProps {
 export const ProductDetails = ({ data }: ProductProps) => {
   return (
     <>
-      <Head>
-        <title>{data.title}</title>
-      </Head>
+      <NextSeo
+        title={data.title}
+        description={data.description}
+        canonical={`https://zaisteprogramuj-sklep.vercel.app/products/${data.id}`}
+        openGraph={{
+          url: `https://zaisteprogramuj-sklep.vercel.app/products/${data.id}`,
+          title: data.title,
+          description: data.description,
+          images: [
+            {
+              url: data.imageUrl,
+              alt: data.imageAlt,
+              type: 'image/jpeg',
+            },
+          ],
+          site_name: 'Random Shop',
+        }}
+      />
       <div className="mt-5">
         <div className="flex flex-col">
           <div className="bg-white p-4">
