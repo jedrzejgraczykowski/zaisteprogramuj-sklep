db3589afd0a48533ff58d615c0cd2e890ef60943
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2022-03-24 23:24:09 +0100

    Initial approach to enhance SEO

--- app/components/ProductDetails.tsx
+++ app/components/ProductDetails.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Rating } from "./Rating";
 import ReactMarkdown from "react-markdown";
+import Head from "next/head";
 
 export interface ProductDetails {
   id: number,
@@ -18,27 +19,32 @@ interface ProductProps {
 
 export const ProductDetails = ({ data }: ProductProps) => {
   return (
-    <div className="mt-5">
-      <div className="flex flex-col">
-        <div className="bg-white p-4">
-          <Image
-            src={data.imageUrl}
-            alt={data.imageAlt}
-            layout="responsive"
-            width={16}
-            height={9}
-            objectFit="contain"
-          />
-        </div>
-        <div>
-          <h2 className="p-4 text-3xl font-bold">{data.title}</h2>
-          <p className="p-4">{data.description}</p>
-          <article className="prose lg:prose-xl p-4">
-            <ReactMarkdown>{data.longDescription}</ReactMarkdown>
-          </article>
-          <Rating rating={data.rating} />
+    <>
+      <Head>
+        <title>{data.title}</title>
+      </Head>
+      <div className="mt-5">
+        <div className="flex flex-col">
+          <div className="bg-white p-4">
+            <Image
+              src={data.imageUrl}
+              alt={data.imageAlt}
+              layout="responsive"
+              width={16}
+              height={9}
+              objectFit="contain"
+            />
+          </div>
+          <div>
+            <h2 className="p-4 text-3xl font-bold">{data.title}</h2>
+            <p className="p-4">{data.description}</p>
+            <article className="prose lg:prose-xl p-4">
+              <ReactMarkdown>{data.longDescription}</ReactMarkdown>
+            </article>
+            <Rating rating={data.rating} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="flex justify-center max-w-7xl mx-auto w-full bg-gray-700 text-white py-3">
-      Jędrzej Graczykowski | Random Shop | 2022
+      Jędrzej Graczykowski | Zaiste Programuj Shop | 2022
     </footer>
   );
 }
