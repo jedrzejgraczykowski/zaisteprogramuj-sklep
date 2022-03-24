4ba48f468596e43cbe2109a20f00d45de803e6c1
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2022-03-25 00:30:22 +0100

    Use generic nex-seo component

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+const title = 'Random Shop';
+const description = 'Random Shop';
+
+const NextSeoConfig = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    site_name: title,
+  },
+};
+
+export default NextSeoConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Layout } from "../app/components/layout/Layout";
+import { DefaultSeo } from "next-seo";
+import NextSeoConfig from '../next-seo.config';
 
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo {...NextSeoConfig} />
       <QueryClientProvider client={client}>
         <Component {...pageProps} />
       </QueryClientProvider>
